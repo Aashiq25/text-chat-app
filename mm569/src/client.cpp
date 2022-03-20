37870c34839b058708da7f79a8d1cd34a494509e
Aashiq_25 <muhamedaashiqtce@gmail.com>
2022-03-19 20:02:09 -0400

    Adding listener in client

--- mm569/src/client.cpp
+++ mm569/src/client.cpp
@@ -28,7 +28,7 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <netdb.h>
-#include<iostream>
+#include <iostream>
 
 #include "../include/client.h"
 #include "../include/logger.h"
@@ -37,6 +37,7 @@
 #define TRUE 1
 #define MSG_SIZE 256
 #define BUFFER_SIZE 1024
+#define STDIN 0
 
 /**
  * main function
@@ -46,7 +47,8 @@
  * @return 0 EXIT_SUCCESS
  */
 
-Client::Client(int argc, char **argv) {
+Client::Client(int argc, char **argv)
+{
 	client_port = std::string(argv[2]);
 	isLoggedIn = false;
 	server = -1;
@@ -54,61 +56,111 @@ Client::Client(int argc, char **argv) {
 
 int Client::InitClient()
 {
-	while (TRUE)
+	int head_socket, selret, sock_index;
+	fd_set master_list, watch_list;
+
+	/* Zero select FD sets */
+	FD_ZERO(&master_list);
+	FD_ZERO(&watch_list);
+
+	/* Register STDIN */
+	FD_SET(STDIN, &master_list);
+
+	head_socket = 0;
+	while (true)
 	{
+		memcpy(&watch_list, &master_list, sizeof(master_list));\
 		cse4589_print_and_log("[PA1-Client@CSE489/589]$ ");
-		// fflush(stdout);
-
-		char *msg = (char *)malloc(sizeof(char) * MSG_SIZE);
-		memset(msg, '\0', MSG_SIZE);
-		if (fgets(msg, MSG_SIZE - 1, stdin) == NULL) // Mind the newline character that will be written to msg
-			exit(-1);
-
-		printf("I got: %s(size:%lu chars)\n", msg, strlen(msg));
-		
-		bool requestServer;
-		std::string input_command(msg);
-		trim(input_command);
-		if (input_command == "AUTHOR") {
-			PrintAuthor(input_command);
-		} else if (input_command == "IP") {
-			PrintIpAddress(input_command);
-		} else if (input_command == "PORT") {
-			PrintClientPortNumber(input_command);
-		} else if (input_command.substr(0, 5) == "LOGIN") {
-			std::size_t ip_seperator = input_command.find(" "), port_seperator;
-			port_seperator = input_command.find(" ", ip_seperator + 1);
-  			std::string server_ip = input_command.substr(ip_seperator + 1, port_seperator - ip_seperator - 1);
-			std::string server_port = input_command.substr(port_seperator + 1);
-			server = ConnectToHost(server_ip, server_port);
-			requestServer = true;
-		}
-
-		if (isLoggedIn) {
-			if (input_command == "LIST") {
-				PrintClientsList(availableClients, input_command);
-			} else if (input_command == "REFRESH") {
-				requestServer = true;
-			} else if (input_command.substr(0, 4) == "SEND") {
-				SendMessage(std::string(input_command));
-			}
-		}
-		if (requestServer && server != -1 && send(server, msg, strlen(msg), 0) == strlen(msg)) {
-			printf("Done!\n");
-		}
-
 		fflush(stdout);
-		/* Initialize buffer to receieve response */
-		char *buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-		memset(buffer, '\0', BUFFER_SIZE);
-		if (requestServer && recv(server, buffer, BUFFER_SIZE, 0) != -1)
+		/* select() system call. This will BLOCK */
+		selret = select(head_socket + 1, &watch_list, NULL, NULL, NULL);
+		if (selret < 0)
+			perror("select failed.");
+
+		/* Check if we have sockets/STDIN to process */
+		if (selret > 0)
 		{
-			ParseAvailableClients(std::string(buffer));
-			printf("Server responded: %s", buffer);
-			requestServer = false;
-			fflush(stdout);
-		} else {
-			perror("Error occurred while receiving");
+			/* Loop through socket descriptors to check which ones are ready */
+			for (sock_index = 0; sock_index <= head_socket; sock_index += 1)
+			{
+				if (FD_ISSET(sock_index, &watch_list))
+				{
+					/* Check if new command on STDIN */
+					if (sock_index == STDIN)
+					{
+
+						char *msg = (char *)malloc(sizeof(char) * MSG_SIZE);
+						memset(msg, '\0', MSG_SIZE);
+						if (fgets(msg, MSG_SIZE - 1, stdin) == NULL) // Mind the newline character that will be written to msg
+							exit(-1);
+
+						printf("I got: %s(size:%lu chars)\n", msg, strlen(msg));
+
+						std::string input_command(msg);
+						trim(input_command);
+						if (input_command == "AUTHOR")
+						{
+							PrintAuthor(input_command);
+						}
+						else if (input_command == "IP")
+						{
+							PrintIpAddress(input_command);
+						}
+						else if (input_command == "PORT")
+						{
+							PrintClientPortNumber(input_command);
+						}
+						else if (input_command.substr(0, 5) == "LOGIN")
+						{
+							std::size_t ip_seperator = input_command.find(" "), port_seperator;
+							port_seperator = input_command.find(" ", ip_seperator + 1);
+							std::string server_ip = input_command.substr(ip_seperator + 1, port_seperator - ip_seperator - 1);
+							std::string server_port = input_command.substr(port_seperator + 1);
+							server = ConnectToHost(server_ip, server_port);
+							FD_SET(server, &master_list);
+                            head_socket = server;
+
+						}
+
+						if (isLoggedIn)
+						{
+							if (input_command == "LIST")
+							{
+								PrintClientsList(availableClients, input_command);
+							}
+							else if (input_command == "REFRESH")
+							{
+								if (server != -1 && send(server, msg, strlen(msg), 0) == strlen(msg))
+								{
+									printf("Refresh requested!\n");
+								}
+							}
+							else if (input_command.substr(0, 4) == "SEND")
+							{
+								SendMessage(std::string(input_command));
+							}
+						}
+					}
+					else if (sock_index == server)
+					{
+						// Incoming messages
+
+						char *buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+						memset(buffer, '\0', BUFFER_SIZE);
+						if (recv(server, buffer, BUFFER_SIZE, 0) > 0)
+						{
+							printf("Server responded: %s", buffer);
+							ParseAvailableClients(std::string(buffer));
+							fflush(stdout);
+						}
+						else
+						{
+							perror("Error occurred while receiving");
+						}
+						free(buffer);
+					}
+				}
+			}
 		}
 	}
 }
@@ -136,10 +188,10 @@ int Client::ConnectToHost(std::string server_ip, std::string server_port)
 	fdsocket = socket(client_addr->ai_family, client_addr->ai_socktype, client_addr->ai_protocol);
 	if (fdsocket < 0)
 		perror("Failed to create socket");
-	
+
 	if (bind(fdsocket, client_addr->ai_addr, client_addr->ai_addrlen) < 0)
 		perror("Bind failed");
-	
+
 	if (connect(fdsocket, server_addr->ai_addr, server_addr->ai_addrlen) < 0)
 		perror("Connect failed");
 
@@ -150,64 +202,72 @@ int Client::ConnectToHost(std::string server_ip, std::string server_port)
 	return fdsocket;
 }
 
-
-void Client::PrintClientPortNumber(std::string cmd) {
+void Client::PrintClientPortNumber(std::string cmd)
+{
 	cse4589_print_and_log("[%s:SUCCESS]\n", cmd.c_str());
 	cse4589_print_and_log("PORT:%d\n", client_port.c_str());
 }
 
-
-void Client::ParseAvailableClients(std::string msg) {
-	if (msg.size() == 0) {
+void Client::ParseAvailableClients(std::string msg)
+{
+	if (msg.size() == 0)
+	{
 		return;
 	}
 	availableClients.clear();
 	std::string connected_str = "Connected Clients:[";
-    std::size_t startIndex = msg.find(connected_str);
-    if (startIndex != -1) {
-        startIndex += connected_str.size();
-    }
-    std::string availableClientsStr = msg.substr(startIndex, msg.find("]", startIndex) - startIndex);
-    
-    std::size_t strSeperator1 = 0;
-    std::size_t strSeperator2;
-    while (strSeperator1 != -1)
-    {
-        strSeperator2 = availableClientsStr.find("\n", strSeperator1);
-        std::string clientStr = availableClientsStr.substr(strSeperator1,
-                                                           (strSeperator2 != -1 ? strSeperator2 - strSeperator1
-                                                                                : availableClientsStr.size() - strSeperator1));
+	std::size_t startIndex = msg.find(connected_str);
+	if (startIndex != -1)
+	{
+		startIndex += connected_str.size();
+	}
+	std::string availableClientsStr = msg.substr(startIndex, msg.find("]", startIndex) - startIndex);
 
-        ClientMetaInfo ct;
-        ct.stringToCMI(clientStr);
-        availableClients.push_back(ct);
+	std::size_t strSeperator1 = 0;
+	std::size_t strSeperator2;
+	while (strSeperator1 != -1)
+	{
+		strSeperator2 = availableClientsStr.find("\n", strSeperator1);
+		std::string clientStr = availableClientsStr.substr(strSeperator1,
+														   (strSeperator2 != -1 ? strSeperator2 - strSeperator1
+																				: availableClientsStr.size() - strSeperator1));
 
-        strSeperator1 = (strSeperator2 != -1 ? strSeperator2 + 1 : -1);
-    }
+		ClientMetaInfo ct;
+		ct.stringToCMI(clientStr);
+		availableClients.push_back(ct);
+
+		strSeperator1 = (strSeperator2 != -1 ? strSeperator2 + 1 : -1);
+	}
 }
 
-
-void Client::SendMessage(std::string msg) {
+void Client::SendMessage(std::string msg)
+{
 	std::string cmd = msg.substr(0, 4);
-    std::size_t ipStart = msg.find(" ") + 1;
-    std::size_t ipEnd = msg.find(" ", ipStart);
-    std::string ipAddress = msg.substr(ipStart + 1, ipEnd - ipStart - 1), message = msg.substr(ipEnd + 1);
+	std::size_t ipStart = msg.find(" ") + 1;
+	std::size_t ipEnd = msg.find(" ", ipStart);
+	std::string ipAddress = msg.substr(ipStart, ipEnd - ipStart), message = msg.substr(ipEnd + 1);
 	bool didSend = false;
-	if (ClientExists(ipAddress)) {
-		if (server != -1 && send(server, msg.c_str(), msg.size(), 0) == msg.size()) {
+	printf("Client ip %s\n", ipAddress.c_str());
+	if (ClientExists(ipAddress))
+	{
+		printf("Client exists");
+		if (server != -1 && send(server, msg.c_str(), msg.size(), 0) == msg.size())
+		{
 			cse4589_print_and_log("[%s:SUCCESS]\n", cmd.c_str());
 			didSend = true;
 		}
 	}
 
 	PrintEndCommand(!didSend, msg);
-
 }
 
-bool Client::ClientExists(std::string& ipAddress) {
-	for (int i = 0; i < availableClients.size(); i++) {
+bool Client::ClientExists(std::string &ipAddress)
+{
+	for (int i = 0; i < availableClients.size(); i++)
+	{
 		ClientMetaInfo client = availableClients[i];
-		if (client.ipAddress == ipAddress) {
+		if (client.ipAddress == ipAddress)
+		{
 			return true;
 		}
 	}
